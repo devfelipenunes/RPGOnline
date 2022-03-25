7f81575e845052a04b8d7ab5d225574d25948359
Dev Felipe Nunes <81275678+devfelipenunes@users.noreply.github.com>
2022-03-24 23:50:48 -0300

    Update RPGOnlineBD.sql

--- RPGOnlineBD.sql
+++ RPGOnlineBD.sql
@@ -1,57 +1,98 @@
---dql
-Create database SenaiExercicio
+--DDL
+CREATE DATABASE RpgOnline
 
-Use SenaiExercicio
+ USE RpgOnline
 
-create table usuario(
-	UsuarioID Int primary key identity,
-	Email varchar (100) unique not null,
-	Senha varchar (255) unique not null
-	)
+ CREATE TABLE Usuarios(
+    UsuarioId INT PRIMARY KEY IDENTITY
+	,Email VARCHAR(100) UNIQUE NOT NULL
+	,Senha VARCHAR(255) NOT NULL
+ )
 
-create table classes(
-	ClasseID int primary key identity,
-	Nome Varchar (50) unique not null,
-	Descricao varchar (255)
-	)
-
-create table Personagens(
-	PersonagemID int primary key identity,
-	NomePersonagem varchar (20) unique not null,
-	Usuarioid int unique foreign key references usuario(UsuarioID),
-	ClasseID int foreign key references classes(ClasseID),
-	)
-
-create table Habilidades(
-	HabilidadeID int primary key identity,
-	Nome varchar (100) unique not null
-	)
-
-create table ClasseHabilidade(
-	ClasseID INT FOREIGN KEY REFERENCES classes(ClasseId),
-	HabilidadeID INT FOREIGN KEY REFERENCES Habilidades(HabilidadeID)
+ CREATE TABLE Classes(
+    ClasseId INT PRIMARY KEY IDENTITY
+	,Nome VARCHAR(50)UNIQUE NOT NULL
+	,Descricao VARCHAR(255)
 )
 
+ CREATE TABLE Personagens(
+    PersonagemId INT PRIMARY KEY IDENTITY
+	,NomePersonagem VARCHAR(15) UNIQUE NOT NULL
+	,UsuarioId INT UNIQUE FOREIGN KEY REFERENCES Usuarios(UsuarioId)
+	,ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId)
+ )
 
---dml
 
-insert into usuario values ('email@email.com', '654321')
-insert into classes values ('barbaro', 'Descricao do barbaro')
-insert into Habilidades values ('Lanca Mortal'), ('Escudo Supremo')
-insert into Personagens (NomePersonagem, UsuarioID, ClasseID) values ('DeuBug' , 1 , 1)
+ CREATE TABLE Habilidades(
+     HabilidadeId INT PRIMARY KEY IDENTITY
+	 ,Nome VARCHAR(100) UNIQUE NOT NULL
+)
 
-insert into usuario values ('email2@email.com' , '321645')
-insert into classes values ('Monge' , 'Descricao do Monge')
-insert into Habilidades values ('Recuperar vida')
-insert into Personagens (NomePersonagem, UsuarioID, ClasseID) values ('Bitbug' , 2 , 2)
+CREATE TABLE ClasseHabilidade(
+    ClasseId INT FOREIGN KEY REFERENCES Classes(ClasseId)
+	,HabilidadeId INT FOREIGN KEY REFERENCES Habilidades(HabilidadeId)
+)
+--DML
 
---dql
 
-select * from usuario
-select * from classes
-select * from Habilidades
-select * from Personagens
+INSERT INTO Usuarios VALUES ('email0@email.com','123456')
+INSERT INTO Classes VALUES ('Bárbaro','Descrição do Bárbaro')
+INSERT INTO Habilidades VALUES ('Lança Mortal'),('Escudo Supremo')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('DeuBug', 1, 1)
+INSERT INTO ClasseHabilidade (ClasseId, HabilidadeId) VALUES (1, 1), (1, 2)
 
-select * from Personagens 
-inner join classes 
-on Personagens.ClasseID = classes.ClasseID
+INSERT INTO Usuarios VALUES ('email2@email.com','senha1234')
+INSERT INTO Classes VALUES ('Monge','Descrição do Monge')
+INSERT INTO Habilidades VALUES ('Recuperar Vida')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('BitBug', 2, 2)
+INSERT INTO ClasseHabilidade (ClasseId, HabilidadeId) VALUES (2, 2), (2, 3)
+
+--DQL
+
+SELECT * FROM Usuarios
+SELECT * FROM Classes
+SELECT * FROM Habilidades
+SELECT * FROM Personagens
+SELECT * FROM ClasseHabilidade
+
+
+SELECT * FROM Personagens
+INNER JOIN Classes
+ON Personagens.ClasseId = Classes.ClasseId
+
+SELECT P.NomePersonagem,C.Nome AS 'Nome da Classe',C.Descricao FROM Personagens AS P
+INNER JOIN Classes AS C 
+ON P.ClasseId = C.ClasseId
+
+
+--DML AO3 UC10
+INSERT INTO Usuarios VALUES ('email3@email.com','123456')
+INSERT INTO Classes VALUES ('Tank','Descrição do Tank')
+INSERT INTO Habilidades VALUES ('Defesa Mortal'),('Avanço Infinito')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('BigBug', 3, 3)
+
+INSERT INTO Usuarios VALUES ('email1@email.com','123456')
+INSERT INTO Classes VALUES ('Ninja','Descrição do Ninja')
+INSERT INTO Habilidades VALUES ('Kunai Mortal'),('Segredo Ninja')
+INSERT INTO Personagens (NomePersonagem, UsuarioId, ClasseId) VALUES ('BatBug', 4, 6)
+
+--Mudando senha de Usuario Especifico AO3 UC10
+UPDATE  Usuarios
+SET Senha = 654321
+WHERE UsuarioId = 4
+
+--Criando Usuario AO3 UC10
+INSERT INTO Usuarios VALUES ('email4@email.com','123456')
+--Deletando Usuario Especifico  AO3 UC10
+DELETE FROM Usuarios WHERE UsuarioId = 5 
+
+--Criando login AO4 UC10
+CREATE LOGIN Jogador
+WITH PASSWORD = '123456'
+
+CREATE USER Jogador FOR LOGIN Jogador
+
+GRANT SELECT TO Jogador 
+
+--Opção permitida para Login "JOGADOR" AO4 UC10
+SELECT * FROM Personagens
